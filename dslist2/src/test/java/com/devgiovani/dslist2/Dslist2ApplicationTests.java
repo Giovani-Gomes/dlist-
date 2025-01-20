670e98d1f50bd9016ac72c908540f002b1cdc5fa
Giovani-Gomes <giovanigomes2002@gmail.com>
2025-01-20 10:37:37 -0300

    Projeto Criado

---
+++ dslist2/src/test/java/com/devgiovani/dslist2/Dslist2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgiovani.dslist2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dslist2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
